2c92b76dca085f685f02cddc9ef0e79a5da9469a
Cường Trần <cuongbk.124@gmail.com>
2019-03-29 13:36:10 +0700

    fix issue

--- app/src/main/java/com/cuong/dialoglib/MainActivity.java
+++ app/src/main/java/com/cuong/dialoglib/MainActivity.java
@@ -3,11 +3,27 @@ package com.cuong.dialoglib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.cuong.awesomelib.AwesomeErrorDialog;
+import com.cuong.awesomelib.interfaces.Closure;
+
 public class MainActivity extends AppCompatActivity {
+
+    AwesomeErrorDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dialog = new AwesomeErrorDialog(this);
+
+        dialog.setMessage("Co loi xay ra")
+                .setErrorButtonClick(new Closure() {
+                    @Override
+                    public void exec() {
+                        dialog.hide();
+                    }
+                })
+                .show();
     }
 }
